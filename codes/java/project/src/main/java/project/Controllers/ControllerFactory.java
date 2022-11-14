d4c042b5b9119e9f63207ff727feeb8cd16c0334
happym <thehappyboy_m@outlook.com>
2022-11-14 15:55:46 -0500

    udpate bridge design pattern figure

--- codes/java/project/src/main/java/project/Controllers/ControllerFactory.java
+++ codes/java/project/src/main/java/project/Controllers/ControllerFactory.java
@@ -9,13 +9,13 @@ public class ControllerFactory {
 
     public static Controller create(String s) {
         switch (s.toLowerCase()) {
-            case "country":
+            case "countries":
                 return new CountryController();
-            case "city":
+            case "cities":
                 return new CityController();
-            case "airline":
+            case "airlines":
                 return new AirlineController();
-            case "flight":
+            case "flights":
                 return new FlightController();
         }
         return null;
